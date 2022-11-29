e88be90c70c4850efe545ce40901421ad8548a77
Kirk <5759798+kirkcoley@users.noreply.github.com>
2022-11-29 08:22:09 -0600

    Update StreamAg.py
    add comments

--- StreamAg.py
+++ StreamAg.py
@@ -2,6 +2,7 @@
 import sys
 from subprocess import PIPE, Popen
 
+# Function to run bash cmds + return the output
 def cmdline(command):
     process = Popen(
         args=command,
@@ -11,15 +12,21 @@ def cmdline(command):
     )
     return process.communicate()[0]
 
+# Take pcap filename from cmd line
 pcap_file = sys.argv[1]
+
 output_file_name = pcap_file.split('.')[0]+'_streams.txt'
 output_file = open(output_file_name, 'w')
+# Count the streams
 count = 0
 while True:
+    # Use the -z option to follow tcp traffic between nodes, ouput ascii and use 'count' as the stream index
     cmd = 'tshark -r %s -z follow,tcp,ascii,%s'%(pcap_file,count)
+    # Run the tshark cmd and add lines to the output to increase readability
     stream = cmdline(cmd)
     stream = stream.split('==============================================================================\n')[1]
     stream += "\n--------------------\n"
+    # Test to see if we should keep going or not
     if 'Node 0: :0' not in stream:
         output_file.write(stream)
     else:
